8e90cff7c447d93850c99309dfbe45995b07e3ca
Jonathan Alicea <jonathan@boldideas.net>
2023-01-27 00:32:40 -0600

    init

---
+++ src/layouts/applications/data-tables/data/dataTableData.ts
@@ -0,0 +1,99 @@
+/**
+=========================================================
+* Material Dashboard 2 PRO React TS - v1.0.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-2-pro-react-ts
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+const dataTableData = {
+  columns: [
+    { Header: "Asset Name", accessor: "assetName", width: "20%" },
+    { Header: "Vendor Name", accessor: "vendorName", width: "20%" },
+    // { Header: "office", accessor: "office", width: "20%" },
+    // { Header: "start date", accessor: "startDate" },
+    { Header: "Risk Level", accessor: "riskLevel" },
+  ],
+
+  rows: [
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+   {
+    assetName: "VQ OS",
+    vendorName: 'vQuip Solutions', 
+    riskLevel: 'high'
+   },
+  ],
+};
+
+export default dataTableData;
